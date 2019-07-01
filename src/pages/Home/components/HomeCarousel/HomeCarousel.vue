e0ea41ac7eaec65154b013875a332f8f03217971
Valerii Kupyna <vkupy@softserveinc.com>
2019-07-01 13:09:53 +0300

    Home page

---
+++ src/pages/Home/components/HomeCarousel/HomeCarousel.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-carousel>
+        <v-carousel-item
+                v-for="(dataItem) in welcomeData"
+                :key="dataItem.id"
+                :src="dataItem.imgUrl"
+        ></v-carousel-item>
+    </v-carousel>
+</template>
+
+<script>
+     const HomecCrousel = {
+         computed: {
+             welcomeData() {
+                 console.log(this);
+
+                 return [];
+             }
+         }
+    };
+
+    export default HomecCrousel;
+</script>
